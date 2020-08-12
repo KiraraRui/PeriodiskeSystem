5eef0eabb0f53127b00e2f364af87c113c6a0c1a
kira achen <kira.kira@live.dk>
2020-08-12 23:11:28 +0200

    update

--- PeriodicTable/src/app/app.component.ts
+++ PeriodicTable/src/app/app.component.ts
@@ -10,3 +10,13 @@ export class AppComponent {
   title = 'PeriodicTable';
   service : ElementService = new ElementService();
 }
+
+/*
+map aka konventere det til orm aka et element inde i element.service.ts
+element inde i angular class
+from json to element objekter
+
++ læs op på decorators 
+https://codecraft.tv/courses/angular/es6-typescript/decorators/
+husk det skal være angular typescript <3
+*/
