209573a8e394cdb02923187d0110f050534bf69b
kira achen <kira.kira@live.dk>
2020-08-12 21:10:24 +0200

    Initial commit

---
+++ PeriodicTable/src/app/elements.ts
@@ -0,0 +1,33 @@
+export class Elements {´
+
+    name: string;
+    phase:string;
+    color:string;
+    symbol: string;
+    summary: string;
+    category: string;
+    appearance: string;
+    atomicMass: number;
+    density: number;
+    period: number;
+    number: number;
+    melt: number;
+    boil: number;
+
+    constructor(name: string, phase:string, color:string, symbol:string, summary:string, category:string, apperarance:string, atomicMass:number, density:number, period:number, number:number, melt:number, boil:number){
+    
+    this.name = name;
+    this.phase = phase;
+    this.color = color;
+    this.symbol = symbol;
+    this.summary = summary;
+    this.category = category;
+    this.appearance = apperarance;
+    this.atomicMass = density;
+    this.period = period;
+    this.number = number;
+    this.melt = melt;
+    this.boil = boil;
+
+    }
+}
